eda06d843152667c8c2d061ebe12441a731e2d85
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-06-01 17:56:23 +0200

    Remove reinforce check

--- lua/groupaitweakdata.lua
+++ lua/groupaitweakdata.lua
@@ -8,28 +8,6 @@ Hooks:PostHook(GroupAITweakData, "init", "init_sss", function (self)
 	self.besiege.assault.force = { force[1] * 0.375, force[2] * 0.375, force[3] * 0.375 }
 	self.besiege.recurring_group_SO.recurring_cloaker_spawn.interval = { math.huge, math.huge }
 
-	local enable_reenforce = true
-	for _, group in pairs(self.besiege.reenforce.groups) do
-		for _, weight in pairs(group) do
-			if weight > 0 then
-				enable_reenforce = false
-				break
-			end
-		end
-		if not enable_reenforce then
-			break
-		end
-	end
-	if enable_reenforce then
-		self.besiege.reenforce.interval = { 60, 45, 30 }
-		self.besiege.reenforce.groups = {
-			tac_swat_shotgun_rush = { 1, 1, 1 },
-			tac_swat_shotgun_flank = { 1, 1, 1 },
-			tac_swat_rifle = { 1, 1, 1 },
-			tac_swat_rifle_flank = { 1, 1, 1 }
-		}
-	end
-
 	self.special_unit_spawn_limits.shield = 2
 	self.special_unit_spawn_limits.medic = 0
 	self.special_unit_spawn_limits.taser = 0
