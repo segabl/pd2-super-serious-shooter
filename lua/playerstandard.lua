7fe1048c3cc797c00974bcfed72615ef3dc5122b
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-06-04 21:45:40 +0200

    Fix assault mode intimidation check not working for clients

--- lua/playerstandard.lua
+++ lua/playerstandard.lua
@@ -1,5 +1,5 @@
 local _get_unit_intimidation_action_original = PlayerStandard._get_unit_intimidation_action
 function PlayerStandard:_get_unit_intimidation_action(enemies, civilians, teammates, specials_only, ...)
-	specials_only = not managers.groupai:state()._rescue_allowed or specials_only
+	specials_only = managers.groupai:state():get_assault_mode() or specials_only
 	return _get_unit_intimidation_action_original(self, enemies, civilians, teammates, specials_only, ...)
 end
