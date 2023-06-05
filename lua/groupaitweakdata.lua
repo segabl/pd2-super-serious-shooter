e37dfebdac724d5afef18e1a1c604e46f58c59a9
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-06-05 10:35:40 +0200

    Improve disabling of specials

--- lua/groupaitweakdata.lua
+++ lua/groupaitweakdata.lua
@@ -1,5 +1,6 @@
 Hooks:PostHook(GroupAITweakData, "init", "init_sss", function (self)
 	self.smoke_and_flash_grenade_timeout = { 7.5, 15 }
+	self.flash_grenade.timer = 1
 	self.min_grenade_timeout = 10
 
 	self.spawn_cooldown_mul = 0.5
@@ -8,11 +9,9 @@ Hooks:PostHook(GroupAITweakData, "init", "init_sss", function (self)
 	self.besiege.assault.force = { force[1] * 0.375, force[2] * 0.375, force[3] * 0.375 }
 	self.besiege.recurring_group_SO.recurring_cloaker_spawn.interval = { math.huge, math.huge }
 
-	self.special_unit_spawn_limits.shield = 2
-	self.special_unit_spawn_limits.medic = 0
-	self.special_unit_spawn_limits.taser = 0
-	self.special_unit_spawn_limits.tank = 0
-	self.special_unit_spawn_limits.spooc = 0
+	for special in pairs(self.special_unit_spawn_limits) do
+		self.special_unit_spawn_limits[special] = special == "shield" and 2 or 0
+	end
 
 	self.enemy_spawn_groups.marshal_squad = nil
 
