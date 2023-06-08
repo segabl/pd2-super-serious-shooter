994c08be05d60f65e661aa3fbd75e87fbbd6887b
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-06-08 09:35:03 +0200

    Add host side fallback for enemy intimidation check

--- lua/groupaistatebase.lua
+++ lua/groupaistatebase.lua
@@ -4,3 +4,8 @@ end
 
 Hooks:PostHook(GroupAIStateBase, "_init_misc_data", "_init_misc_data_sss", register_special_types)
 Hooks:PostHook(GroupAIStateBase, "on_simulation_started", "on_simulation_started_sss", register_special_types)
+
+local has_room_for_police_hostage_original = GroupAIStateBase.has_room_for_police_hostage
+function GroupAIStateBase:has_room_for_police_hostage(...)
+	return not self:get_assault_mode() and has_room_for_police_hostage_original(self, ...) or false
+end
