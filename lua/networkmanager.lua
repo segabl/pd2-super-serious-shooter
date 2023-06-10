e7e8f9869595330bcadb3f3af930fd8701917dfd
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-06-10 10:54:54 +0200

    Move matchmaking keys to network manager

--- lua/networkmanager.lua
+++ lua/networkmanager.lua
@@ -1,1 +1,2 @@
 NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY .. "_sss_v" .. SuperSeriousShooter.mod_instance:GetVersion()
+NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY .. "_sss_v" .. SuperSeriousShooter.mod_instance:GetVersion()
--- lua/networkmatchmakingepic.lua
+++
@@ -1,1 +0,0 @@
-NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY .. "_sss_v" .. SuperSeriousShooter.mod_instance:GetVersion()
