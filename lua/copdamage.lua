94f9b0eb116c984d1525ace4b339d93929d1251c
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-08-11 19:42:44 +0200

    Add fallback to critical hit check

--- lua/copdamage.lua
+++ lua/copdamage.lua
@@ -1,3 +1,7 @@
 Hooks:PostHook(CopDamage, "accuracy_multiplier", "accuracy_multiplier_sss", function (self)
 	return Hooks:GetReturn() * (self._unit:movement():get_walk_to_pos() and 1 or 1.25)
 end)
+
+function CopDamage:can_be_critical()
+	return false
+end
