6e123b0164e46d311e51c2a65505ad75b6adb72b
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-08-20 13:48:35 +0200

    Make jumping always consume stamina not just while running

--- lua/playerstandard.lua
+++ lua/playerstandard.lua
@@ -3,3 +3,16 @@ function PlayerStandard:_get_unit_intimidation_action(enemies, civilians, teamma
 	specials_only = managers.groupai:state():get_assault_mode() or specials_only
 	return _get_unit_intimidation_action_original(self, enemies, civilians, teammates, specials_only, ...)
 end
+
+local _check_action_jump_original = PlayerStandard._check_action_jump
+function PlayerStandard:_check_action_jump(t, input, ...)
+	local is_running = self._move_dir and self._running and self._unit:movement():is_above_stamina_threshold() and t - self._start_running_t > 0.4
+
+	local new_action = _check_action_jump_original(self, t, input, ...)
+
+	if self._jump_t == t and not is_running then
+		self._unit:movement():subtract_stamina(tweak_data.player.movement_state.stamina.JUMP_STAMINA_DRAIN * 0.5)
+	end
+
+	return new_action
+end
