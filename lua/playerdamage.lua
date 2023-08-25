9ac03db16b6810e32c5a996469a8247603623a86
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-08-26 00:00:27 +0200

    Add grace period on armor break

--- lua/playerdamage.lua
+++ lua/playerdamage.lua
@@ -44,3 +44,21 @@ function PlayerDamage:damage_killzone(attack_data, ...)
 
 	self:_call_listeners(damage_info)
 end
+
+function PlayerDamage:_chk_dmg_too_soon()
+	local next_allowed_dmg_t = type(self._next_allowed_dmg_t) == "number" and self._next_allowed_dmg_t or Application:digest_value(self._next_allowed_dmg_t, false)
+	return managers.player:player_timer():time() < next_allowed_dmg_t
+end
+
+local _calc_armor_damage_original = PlayerDamage._calc_armor_damage
+function PlayerDamage:_calc_armor_damage(...)
+	local had_armor = self:get_real_armor() > 0
+
+	local health_subtracted = _calc_armor_damage_original(self, ...)
+
+	if health_subtracted > 0 and had_armor and self:get_real_armor() <= 0 then
+		self._next_allowed_dmg_t = Application:digest_value(managers.player:player_timer():time() + 0.35, true)
+	end
+
+	return health_subtracted
+end
