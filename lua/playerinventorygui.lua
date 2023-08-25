3e96c55afd1e03f19f7e0ac3ba171dfccfe30785
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-08-26 01:09:09 +0200

    Visually disable skills and perks in the inventory screen

---
+++ lua/playerinventorygui.lua
@@ -0,0 +1,12 @@
+Hooks:PreHook(PlayerInventoryGui, "create_box", "create_box_sss", function (self, params)
+	if params.name == "skilltree" or params.name == "specialization" then
+		params.text = "DISABLED"
+		params.text_selected_color = tweak_data.screen_colors.important_1
+		params.image = "guis/textures/pd2/none_icon"
+		params.texture_rect = nil
+		params.image_rect = nil
+		params.image_size_mul = 1.5
+	elseif params.image == "guis/textures/pd2/inv_skillcards_icons" then
+		params.alpha = 0
+	end
+end)
