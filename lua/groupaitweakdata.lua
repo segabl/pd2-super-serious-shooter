f7c11781d7d6a90c7106a5ede1a1f40a5fa0d715
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-10-10 12:23:49 +0200

    Limit amount of specials in groups
    This is done to limit the amount of shields in shield groups to just have one shield

--- lua/groupaitweakdata.lua
+++ lua/groupaitweakdata.lua
@@ -3,11 +3,20 @@ Hooks:PostHook(GroupAITweakData, "init", "init_sss", function (self)
 	self.min_grenade_timeout = 10
 
 	local force = self.besiege.assault.force
-	self.besiege.assault.force = { force[1] * 0.375, force[2] * 0.375, force[3] * 0.375 }
+	self.besiege.assault.force = { force[1] * 0.35, force[2] * 0.35, force[3] * 0.35 }
 	self.besiege.recurring_group_SO.recurring_cloaker_spawn.interval = { math.huge, math.huge }
 
 	for special in pairs(self.special_unit_spawn_limits) do
 		self.special_unit_spawn_limits[special] = special == "shield" and 2 or 0
+	end
+
+	for _, group in pairs(self.enemy_spawn_groups) do
+		for _, data in pairs(group.spawn) do
+			if self.unit_categories[data.unit].special_type ~= nil then
+				data.amount_max = 1
+				data.amount_min = math.min(data.amount_max, data.amount_min or 0)
+			end
+		end
 	end
 
 	self.enemy_spawn_groups.marshal_squad = nil
