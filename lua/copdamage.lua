3e07cca36350c13e40957fdd5d8bc1faae99e5fb
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-08-19 18:48:38 +0200

    Improve cop movement accuracy check

--- lua/copdamage.lua
+++ lua/copdamage.lua
@@ -1,5 +1,5 @@
 Hooks:PostHook(CopDamage, "accuracy_multiplier", "accuracy_multiplier_sss", function (self)
-	return Hooks:GetReturn() * (self._unit:movement():get_walk_to_pos() and 1 or 1.25)
+	return Hooks:GetReturn() * (self._unit:anim_data().move and 1 or 1.25)
 end)
 
 function CopDamage:can_be_critical()
