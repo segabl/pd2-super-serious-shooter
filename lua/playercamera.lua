285fdbc303b771a71d3a8f12325ee9b9dc598500
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-01-25 15:09:18 +0100

    Increase variation of bullet hit shakers

--- lua/playercamera.lua
+++ lua/playercamera.lua
@@ -1,4 +1,8 @@
 local play_shaker_original = PlayerCamera.play_shaker
 function PlayerCamera:play_shaker(effect, amplitude, ...)
-	return play_shaker_original(self, effect, effect == "player_bullet_damage" and self._damage_bullet_shake_multiplier or amplitude, ...)
+	if effect == "player_bullet_damage" then
+		effect = "player_bullet_damage_var" .. math.random(1, 4)
+		amplitude = self._damage_bullet_shake_multiplier
+	end
+	return play_shaker_original(self, effect, amplitude, ...)
 end
