b420715af103555fcdaaf235e5a7388f201dbf5a
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-03-17 16:49:22 +0100

    Tweak poison damage

--- lua/dottweakdata.lua
+++ lua/dottweakdata.lua
@@ -1,6 +1,6 @@
 Hooks:PostHook(DOTTweakData, "init", "init_sss", function (self)
 	for k, v in pairs(self.dot_entries.poison) do
-		v.dot_damage = (k:match("_cloud$") and 30 or 3) / v.dot_length
+		v.dot_damage = (k:match("^ammo_") and 3 or 30) / v.dot_length
 		v.hurt_animation_chance = 0.25
 		v.dot_tick_period = 1
 	end
