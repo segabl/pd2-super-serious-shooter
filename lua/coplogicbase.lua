0aa117d37931224b3b80e6c27d9251ab86af9ba3
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-10-07 16:53:16 +0200

    Only increase logic update interval in loud

--- lua/coplogicbase.lua
+++ lua/coplogicbase.lua
@@ -1,5 +1,5 @@
 local _upd_attention_obj_detection_original = CopLogicBase._upd_attention_obj_detection
-function CopLogicBase._upd_attention_obj_detection(...)
-	local delay = _upd_attention_obj_detection_original(...)
-	return math.min(0.25, delay)
+function CopLogicBase._upd_attention_obj_detection(data, ...)
+	local delay = _upd_attention_obj_detection_original(data, ...)
+	return data.cool and delay or math.min(0.25, delay)
 end
