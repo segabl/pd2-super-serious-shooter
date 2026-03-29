82b4b7f2bd60b6c2bc772eafb15665d8855fd433
Sebastian Gabl <sebastian.gabl@gmx.at>
2026-03-29 22:56:22 +0200

    Improve consistency of surrender conditions
    Enemies will surrender between assault breaks, when their group is retreating or if they aren't part of a group
    The intimidation failed hint will now show consistently after the second shout if the conditions aren't met

--- lua/coplogicbase.lua
+++ lua/coplogicbase.lua
@@ -3,3 +3,24 @@ function CopLogicBase._upd_attention_obj_detection(data, ...)
 	local delay = _upd_attention_obj_detection_original(data, ...)
 	return data.cool and delay or math.min(0.25, delay)
 end
+
+local _evaluate_reason_to_surrender_original = CopLogicBase._evaluate_reason_to_surrender
+function CopLogicBase._evaluate_reason_to_surrender(data, my_data, aggressor_unit, ...)
+	local hold_chance = _evaluate_reason_to_surrender_original(data, my_data, aggressor_unit, ...)
+	if not hold_chance then
+		return
+	end
+
+	if not managers.groupai:state():get_assault_mode() then
+		return hold_chance
+	end
+
+	local grp_objective = data.group and data.group.objective
+	if not grp_objective or grp_objective.type == "retire" then
+		return hold_chance
+	end
+
+	if data.surrender_window then
+		data.surrender_window.window_expire_t = 0
+	end
+end
--- lua/groupaistatebase.lua
+++ lua/groupaistatebase.lua
@@ -5,10 +5,4 @@ end
 Hooks:PostHook(GroupAIStateBase, "_init_misc_data", "_init_misc_data_sss", register_special_types)
 Hooks:PostHook(GroupAIStateBase, "on_simulation_started", "on_simulation_started_sss", register_special_types)
 
-function GroupAIStateBase:has_room_for_police_hostage()
-	local task_data = self._task_data.assault
-	local is_assault = task_data.active and task_data.phase ~= "fade" and task_data.phase ~= "anticipation"
-	return not is_assault and self._police_hostage_headcount < 4 or false
-end
-
 function GroupAIStateBase:_process_recurring_grp_SO() end
